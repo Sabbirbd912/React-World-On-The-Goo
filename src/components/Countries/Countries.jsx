adb6fc593e9f5b2132019ea5dc4bb7829e709876
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-03-15 12:05:06 +0600

    button test by console

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,20 @@
-import React, { use } from 'react';
-import Country from '../Counrty/Country';
+import React, { use } from "react";
+import Country from "../Counrty/Country";
+import "./Countries.css";
 
-const Countries = ({countriesPromise}) => {
-    const countriesData =use(countriesPromise);
-    const countries=countriesData.countries;
-    return (
-        <div>
-            <h2>In the Countries: {countries.length}</h2>
-            {
-                countries.map(country=><Country country={country}></Country>)
-            }
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  return (
+    <div>
+      <h2>In the Countries: {countries.length}</h2>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
