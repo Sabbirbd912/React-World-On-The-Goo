0574b79072ce9bb38e2c46506930b15724f83a22
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-03-15 12:53:59 +0600

    Conditional Render

--- src/components/Counrty/Country.jsx
+++ src/components/Counrty/Country.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({country}) => {
-    console.log(country.region.region)
+
+    const [visited, setVisited]=useState(false);
+    // console.log(country.region.region)
 
     const handleVisited=()=>{
-        console.log("Button Click")
+        // step 1
+        // if(visited){
+        //     setVisited(false)
+        // }else{
+        //     setVisited(true)
+        // }
+
+        // step 2
+        // setVisited(visited ? false : true);
+
+        // step 3
+        setVisited(!visited);
     }
 
     return (
-        <div className='country'>
+        <div className={
+            `country ${visited && 'country-visited'}`
+        }>
             <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population:{country.population.population}</p>
             <p>Location: {country.region.region} </p>
             <p>Area:{country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button onClick={handleVisited}>{visited ? "Visited" : "Not Visited"}</button>
         </div>
     );
 };
